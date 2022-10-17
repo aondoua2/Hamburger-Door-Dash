9c788bbb16eb58bf87fb59cf506006732d89afdb
zolsen10 <soccerzack10@yahoo.com>
2022-10-17 11:28:16 -0600

    Base Code

--- hamburger.py
+++ hamburger.py
@@ -1,2 +1,31 @@
 # Oliver Escobar
 # Karl Nordgren
+
+import random
+
+class Order:
+    def __init__(self, randBurger):
+        self.burger_count = 0
+        randBurger = randBurger
+
+    def randomBurgers(self, randBurg):
+        randBurg = random.randrange(1,21)
+        return randBurg
+        
+class Person:
+    def __init__(self, customer_name):
+        self.customer_name = customer_name
+
+    def customerName(self):
+        self.asCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander", "Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
+
+class Customer(Person):
+    def __init__(self, order):
+        super().__init__()
+        self.order = order
+
+
+
+order = Order()
+ranbBurg = order.randomBurgers(0)
+order(randBurg)
