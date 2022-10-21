98bb6d65aa4069d3b17d22452eb717e08655a795
oliverescobar <103139909+oliverescobar@users.noreply.github.com>
2022-10-20 22:00:57 -0600

    Retitled and added Comments

--- hamburgerDoorDash.py
+++ hamburgerDoorDash.py
@@ -3,20 +3,24 @@
 
 import random
 
-# Imported from original code (Hamburger.py)
+# First Class
 class Order:
     def __init__(self):
+       # Assigning the attribute to the method
         self.burgerCount = self.randomBurger()
 
+        # Method to assigning burgers
     def randomBurger(self):
         self.burgerCount = random.randrange(1, 21)
         return self.burgerCount
 
-
+# Second Class!
 class Person:  # CHANGE PERSON CLASS TO INIT TO RANDOM
     def __init__(self):
+        # Assigning the attribute to the method
         self.customerName = self.randomName()
 
+        # Method to randomize customers
     def randomName(self):
         self.asCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander", "Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
         customerName = random.choice(self.asCustomers)
@@ -25,29 +29,33 @@ class Person:  # CHANGE PERSON CLASS TO INIT TO RANDOM
 
 class Customer(Person):
     def __init__(self):
+        # Super function that calls orders and customers.
         super().__init__()
         self.order = Order()
 
 # This is a dictionary data type. It holds the values for the customers' names and the number of orders.
+# Queue and Dictionary inputs.
 queueCustomers = []
 dictCustomer = {}
 newCustomer = 100
 firstCustomer = Customer()
 
 # This is the code for the Queue.
-for iCount in range(1, newCustomer + 1):  # It is plus one because it is not inclusive right?
+for iCount in range(1, newCustomer + 1): 
     iRandomName = Customer()
     queueCustomers.append(iRandomName)
     # This is the code that adds Dictionary inputs.
 
+# New code that adds to dictionary and adds the amount of burgers.
 while len(queueCustomers) > 0:
     if queueCustomers[0].customerName in dictCustomer:
+        # We need to call the dictCustomer and then place them in []
         dictCustomer[queueCustomers[0].customerName] += queueCustomers[0].order.burgerCount
     else:
         dictCustomer[queueCustomers[0].customerName] = queueCustomers[0].order.burgerCount
     queueCustomers.pop(0)
 
-    # Print out each customer and their total burgers ordered sorted by the most number of burgers ordered
+# Print out each customer and their total burgers ordered sorted by the most number of burgers ordered
 print("\n", "".ljust(5), "Top Customers")
 print("-" * 27)
 listSortedCustomers = sorted(dictCustomer.items(), key=lambda x: x[1], reverse=True)
